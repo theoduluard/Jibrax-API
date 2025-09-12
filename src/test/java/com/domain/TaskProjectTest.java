fe55bd86039b004663425b7358c0aaa8aee9ad07
tduluard <tduluard@horizontrading.io>
2025-09-12 16:13:45 +0200

    @devops - Test code coverage badge by adding useless junit test

--- src/test/java/com/domain/TaskProjectTest.java
+++ src/test/java/com/domain/TaskProjectTest.java
@@ -1,4 +1,13 @@
 package com.domain;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TaskProjectTest {
+
+    @Test
+    public void simpleTest(){
+        assertEquals(1, 0+1);
+    }
 }
