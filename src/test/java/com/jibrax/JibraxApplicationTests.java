e4dc06efdf66be0fb40ef4a518d3cde49c3f9314
tduluard <theo.duluard@etudiant.univ-rennes.fr>
2026-05-14 14:47:20 +0200

    Update blocking test

--- src/test/java/com/jibrax/JibraxApplicationTests.java
+++ src/test/java/com/jibrax/JibraxApplicationTests.java
@@ -1,13 +1,8 @@
 package com.jibrax;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class JibraxApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
